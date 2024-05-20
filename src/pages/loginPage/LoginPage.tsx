425034687e18176bf970cecc8c7c417f3dc05abb
dyzioq <dycha2217@gmail.com>
2024-05-20 21:10:50 +0200

    signup and login update(still only styles)

--- src/pages/loginPage/LoginPage.tsx
+++ src/pages/loginPage/LoginPage.tsx
@@ -1,15 +1,49 @@
-import Header from "../../components/header/Header";
+import { useState } from "react";
 import "./LoginPage.scss";
-//import "./LoginPage.css";
 import { useNavigate } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function LoginPage() {
   const navigate = useNavigate();
 
-  const handleSubmit = (event: React.FormEvent) => {
+  const [inputEmail, setInputEmail] = useState<string>("");
+  const [inputPassword, setInputPassword] = useState<string>("");
+
+  const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
     // Dodaj logikę obsługi formularza tutaj
     console.log("Form submitted");
+
+    if (inputEmail !== "john@example.com") {
+      toast.error("Incorrect email or password!");
+    } else if (inputPassword !== "Polo1#") {
+      toast.error("Incorrect email or password!");
+    } else {
+      navigate("/student");
+    }
+
+    try {
+      const response = await fetch("https://localhost:7066/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: inputEmail,
+          password: inputPassword,
+        }),
+      });
+
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      const result = await response.json();
+      console.log("Success:", result);
+    } catch (error) {
+      console.error("Error:", error);
+    }
   };
 
   return (
@@ -19,11 +53,25 @@ export default function LoginPage() {
         <form onSubmit={handleSubmit}>
           <div>
             <label htmlFor="username">Username</label>
-            <input type="text" id="username" name="username" required />
+            <input
+              value={inputEmail}
+              onChange={(e) => setInputEmail(e.target.value)}
+              type="text"
+              id="username"
+              name="username"
+              required
+            />
           </div>
           <div>
             <label htmlFor="password">Password</label>
-            <input type="password" id="password" name="password" required />
+            <input
+              value={inputPassword}
+              onChange={(e) => setInputPassword(e.target.value)}
+              type="password"
+              id="password"
+              name="password"
+              required
+            />
           </div>
           <button type="submit">Login</button>
         </form>
@@ -33,6 +81,7 @@ export default function LoginPage() {
       </div>
       <button onClick={() => navigate("/student")}>Student</button>
       <button onClick={() => navigate("/teacher")}>Teacher</button>
+      <ToastContainer />
     </>
   );
 }
