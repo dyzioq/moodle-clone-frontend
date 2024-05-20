b8fbb04138f95d574ea8a6a0f8db020e07333e74
dyzioq <dycha2217@gmail.com>
2024-05-20 21:31:23 +0200

    Revert "signup and login update(still only styles)"
    This reverts commit ce8f2c637f6acbad77a89628133fdac40f45d72a.

--- src/pages/signUpPage/SignUpPage.tsx
+++ src/pages/signUpPage/SignUpPage.tsx
@@ -1,42 +1,14 @@
-import { useState } from "react";
+import Header from "../../components/header/Header";
 import "./SignUpPage.scss";
 import { useNavigate } from "react-router-dom";
-import { ToastContainer, toast } from "react-toastify";
 
 export default function SignUpPage() {
   const navigate = useNavigate();
-
-  const [inputEmail, setInputEmail] = useState<string>("");
-  const [inputPassword, setInputPassword] = useState<string>("");
-
-  async function addUser(email: string, password: string) {
-    try {
-      const response = await fetch("https://localhost:7066/register", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          email: email,
-          password: password,
-        }),
-      });
-      console.log(response);
-      const data = await response.json();
-
-      return data;
-    } catch {
-      toast.error("Email or password is incorrect");
-    }
-  }
 
   const handleSubmit = (event: React.FormEvent) => {
     event.preventDefault();
     // Dodaj logikę obsługi formularza tutaj
     console.log("Form submitted");
-
-    addUser(inputEmail, inputPassword);
-    //navigate("/student");
   };
 
   return (
@@ -46,35 +18,20 @@ export default function SignUpPage() {
         <form onSubmit={handleSubmit}>
           <div>
             <label htmlFor="username">Username</label>
-            <input
-              value={inputEmail}
-              onChange={(e) => setInputEmail(e.target.value)}
-              type="text"
-              id="username"
-              name="username"
-              required
-            />
+            <input type="text" id="username" name="username" required />
           </div>
           <div>
             <label htmlFor="password">Password</label>
-            <input
-              value={inputPassword}
-              onChange={(e) => setInputPassword(e.target.value)}
-              type="password"
-              id="password"
-              name="password"
-              required
-            />
+            <input type="password" id="password" name="password" required />
           </div>
           <button type="submit">Sign Up</button>
         </form>
         <a href="" onClick={() => navigate("/")}>
-          Already have an account? Login!
+          Alredy have an account? Login!
         </a>
       </div>
       <button onClick={() => navigate("/student")}>Student</button>
       <button onClick={() => navigate("/teacher")}>Teacher</button>
-      <ToastContainer />
     </>
   );
 }
