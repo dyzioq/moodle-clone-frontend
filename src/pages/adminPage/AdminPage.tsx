0022f6eefd127706dab8b18ac66c4bc4d318c317
dyzioq <dycha2217@gmail.com>
2024-06-25 13:25:56 +0200

    create patch request for changing role, updated admin panel

--- src/pages/adminPage/AdminPage.tsx
+++ src/pages/adminPage/AdminPage.tsx
@@ -1,8 +1,9 @@
 import styles from "../teacherRepoPage/TeacherRepoPage.module.scss";
 import Header from "../../components/header/Header";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { Modal, Form, Button } from "react-bootstrap";
+import { ToastContainer, toast } from "react-toastify";
 
 export default function AdminPage() {
   const navigate = useNavigate();
@@ -12,10 +13,15 @@ export default function AdminPage() {
   const handleCloseEdit = () => setShowEdit(false);
   const handleShowEdit = () => setShowEdit(true);
 
+  const [usersList, setUsersList] = useState([
+    { name: "", surname: "", email: "" },
+  ]);
+  const [userEmail, setUserEmail] = useState("");
+  const [userRole, setUserRole] = useState("");
+
   async function getUsersList() {
     console.log("scaiagam uzytkownikow");
     var token = localStorage.getItem("token") || "a";
-    console.log("tokenadmin" + token);
     token = token.replace(/['"]+/g, "");
     const response = await fetch(
       "https://localhost:7066/api/User/admin/Users",
@@ -28,36 +34,82 @@ export default function AdminPage() {
       }
     );
     const data = await response.json();
-    //console.log(data);
-    //console.log(typeof data);
-
-    // var usersArray = Object.keys(data).map(function (userIndex) {
-    //   let user = data[userIndex];
-    //   // do something with person
-    //   return user;
-    // });
-    // console.log(usersArray);
-    // console.log(typeof usersArray);
+    setUsersList(data);
 
     return data;
   }
 
-  const usersList = getUsersList();
-  usersList.then(function (result) {
-    //console.log(result[1]);
-    return result;
-  });
-  console.log(usersList);
-  // console.log("check");
-  // console.log(usersList);
-  // console.log("objekt :" + typeof usersList);
+  async function getRole(email: string) {
+    try {
+      var token = localStorage.getItem("token") || "a";
+      token = token.replace(/['"]+/g, "");
+      const roleResponse = await fetch(
+        `https://localhost:7066/api/User/roles?email=${email}`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      if (!roleResponse.ok) {
+        toast.error("role is not valid");
+        throw new Error("Network response was not ok");
+      }
+      const roleResult = await roleResponse.json();
+      setUserRole(roleResult);
+      return roleResult;
+    } catch (error) {
+      console.error("Error:", error);
+      console.log("role nie dzialaja");
+    }
+  }
 
-  var list = [
-    { name: "Adam", surname: "Kowalksi" },
-    { name: "Pawel ", surname: "Jakis" },
-    { name: "Bartek ", surname: "Nowak" },
-    { name: "Michal", surname: "Piotrowski" },
-  ];
+  async function patchRole(email: string, role: string) {
+    var token = localStorage.getItem("token") || "a";
+    token = token.replace(/['"]+/g, "");
+
+    try {
+      const response = await fetch("https://localhost:7066/api/User/userRole", {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({
+          userEmail: email,
+          roleName: role,
+        }),
+      });
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const result = await response.json();
+
+      return result;
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  }
+
+  async function showModal(email: string) {
+    setUserEmail(email);
+    getRole(email);
+    handleShowEdit();
+    console.log("modal opened");
+  }
+
+  function handleChangeRole(role: string) {
+    patchRole(userEmail, role);
+    console.log("role changed succesfully");
+
+    handleCloseEdit();
+  }
+
+  useEffect(() => {
+    getUsersList();
+  }, []);
 
   return (
     <>
@@ -68,7 +120,13 @@ export default function AdminPage() {
         <Modal.Body>
           <Form>
             <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-              <Form.Label>Select Role</Form.Label>
+              <p>
+                <Form.Label>Current Role: {userRole}</Form.Label>
+              </p>
+              <p>
+                <Form.Label>Select Role</Form.Label>
+              </p>
+
               <p>
                 <a
                   href=""
@@ -76,8 +134,7 @@ export default function AdminPage() {
                   className="btn btn-secondary popover-test mr-5 my-1"
                   title="Popover title"
                   onClick={() => {
-                    "dac tutaj funkcje zmiany roli na studenta - konczy sie na handle closeedit";
-                    handleCloseEdit();
+                    handleChangeRole("Student");
                   }}
                 >
                   Student
@@ -89,8 +146,7 @@ export default function AdminPage() {
                   className="btn btn-secondary popover-test my-1"
                   title="Popover title"
                   onClick={() => {
-                    "dac tutaj funkcje zmiany roli na studenta - konczy sie na handle closeedit";
-                    handleCloseEdit();
+                    handleChangeRole("Teacher");
                   }}
                 >
                   Teacher
@@ -113,22 +169,27 @@ export default function AdminPage() {
         <div className={styles.repo__header}>
           <h1>List of Users</h1>
         </div>
-        {/* <ul className={styles.tasks__list}>
+        <ul className={styles.tasks__list}>
           {usersList.map((el, index) => (
             <li key={index} className={styles.task}>
               <div className={styles.task__header}>
-                <h2>hej</h2>
+                <h2>
+                  {el.name} {el.surname}, {el.email}
+                </h2>
                 <button
                   className={styles["cbtn--task"]}
-                  onClick={handleShowEdit}
+                  onClick={() => {
+                    showModal(el.email);
+                  }}
                 >
                   Edit Role
                 </button>
               </div>
             </li>
           ))}
-        </ul> */}
+        </ul>
       </div>
+      <ToastContainer />
     </>
   );
 }
